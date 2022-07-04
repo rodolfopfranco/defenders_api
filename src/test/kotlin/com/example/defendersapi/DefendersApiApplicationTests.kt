546dbd1ef7b83b3ca651d2bad2f9a963ec9e299d
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2022-07-04 11:41:46 -0300

    Project Started

---
+++ src/test/kotlin/com/example/defendersapi/DefendersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.defendersapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DefendersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
